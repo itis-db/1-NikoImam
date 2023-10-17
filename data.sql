0036bd3e3a3ea5902d2b0f5c1583774297609cb2
NikoImam <niyaz120904@gmail.com>
2023-10-17 23:51:48 +0300

    Changes in data.sql

--- data.sql
+++ data.sql
@@ -1,17 +1,29 @@
-    insert into activitytype(
-    	  activitytypeid
-        ,name
-        ,sysname
-    )
-    select d.*
-    from (
-    	values(1,'Программа','Program')
-        ,(2,'Подпрограмма','SubProgram')
-        ,(3,'Проект','Project')
-        ,(4,'Контракт','Contract')
-        ,(5,'КТ','Point')
-    ) d(activitytypeid, name, sysname)
-    where not exists(
-    	select 1 from activitytype t
-        where t.activitytypeid = d.activitytypeid
-    );
+insert into activitytype (activitytypeid ,name ,sysname)
+select *
+from (
+	values(1,'Программа','Program'),
+	(2,'Подпрограмма','SubProgram'),
+	(3,'Проект','Project'),
+	(4,'Контракт','Contract'),
+	(5,'КТ','Point')
+) as d(activitytypeid, name, sysname)
+where not exists(
+	select 1 from activitytype as t
+    where t.activitytypeid = d.activitytypeid
+);
+
+--Заполнение таблицы Area
+insert into "area" (areaid, "name")
+select *
+from(
+	values (1, 'Europe'),
+	(2, 'Asia'),
+	(3, 'Africa'),
+	(4, 'Australia'),
+	(5, 'North America'),
+	(6, 'South America')
+) as tmp(areaid, "name")
+where not exists(
+	select 1 from "area" as t
+	where t.areaid = tmp.areaid
+);
